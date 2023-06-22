d1053424a86685bb47dea603cb75c1ae5a771e06
Gustavo Ferreira <70302908+Gussstavof@users.noreply.github.com>
2023-06-22 12:23:32 -0300

    queries

--- part1_DQL_Filters.sql
+++ part1_DQL_Filters.sql
@@ -35,9 +35,28 @@ FROM users
 WHERE birthdate IS NOT null 
 OR name LIKE 'F%a%';
 
+SELECT * FROM users
+ORDER BY users.name DESC;
+
+SELECT * FROM users
+ORDER BY users.birthdate
+OFFSET 3;
+
+SELECT COUNT(*) FROM users;
+SELECT MAX(birthdate) FROM users;
+SELECT MIN(birthdate) FROM users;
+SELECT ROUND(AVG(id), 0) FROM users;
+
+SELECT DISTINCT name FROM users;
+
+SELECT name, COUNT(name) FROM users
+GROUP BY(name);
+
+SELECT name FROM users
+GROUP BY(name)
+HAVING COUNT(name) > 1;
 
 
 
-	
 
 	
